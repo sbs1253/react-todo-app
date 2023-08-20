0c7a52d9727b061b5d32602a4f942ff1f5f623d5
songbbang <rokaf340@gmail.com>
2023-08-20 21:17:01 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -16,22 +16,41 @@ export default class App extends Component {
       textDecoration: 'none',
     };
   };
-  todoData = [
-    {
-      id: '1',
-      title: '공부하기',
-      completed: true,
-    },
-    {
-      id: '2',
-      title: '복습하기',
-      completed: true,
-    },
-  ];
-
+  state = {
+    todoData: [
+      {
+        id: '1',
+        title: '공부하기',
+        completed: true,
+      },
+      {
+        id: '2',
+        title: '복습하기',
+        completed: true,
+      },
+    ],
+    value: '',
+  };
   handleClick = (id) => {
-    const newTodoData = this.todoData.filter((data) => data.id !== id);
+    const newTodoData = this.state.todoData.filter((data) => data.id !== id);
     console.log('newTodoData:', newTodoData);
+    this.setState({ todoData: newTodoData });
+  };
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+    // console.log(e.target.value, this.state.value);
+  };
+  handleSubmit = (e) => {
+    e.preventDafault();
+
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      completed: false,
+    };
+    console.log(newTodo);
+    this.setState({ todoData: [...this.state.todoData, newTodo], value: '' });
   };
   render() {
     return (
@@ -41,7 +60,7 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
 
-          {this.todoData.map((data) => (
+          {this.state.todoData.map((data) => (
             <div style={this.getStyle()} key={data.id}>
               <p>
                 <input type='checkbox' defaultChecked={false} />
@@ -55,6 +74,22 @@ export default class App extends Component {
               </p>
             </div>
           ))}
+          <form style={{ display: 'flex' }} onSubmit={this.handleSubmit}>
+            <input
+              type='text'
+              name='value'
+              style={{ flex: 10, padding: '10px' }}
+              placeholder='해야 할 일 입력'
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <input
+              type='submit'
+              className='btn'
+              style={{ flex: 1 }}
+              value='전송'
+            />
+          </form>
         </div>
       </div>
     );
